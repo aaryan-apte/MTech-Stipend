90e115c65c6938984f6fe04e22b55f6906b96456
sailieg <gholesailie1410@gmail.com>
2023-04-16 16:39:22 +0530

    Attendance Page continued

---
+++ MTech-Stipend-dev/android/app/src/main/kotlin/com/example/mtech_stipend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mtech_stipend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ MTech-Stipend-dev/lib/loginRegister/authenticate.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:mtech_stipend/pages/hod/hod_records.dart';
+
+import 'login.dart';
+
+class Authenticate2 extends StatelessWidget {
+  const Authenticate2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: WillPopScope(
+      onWillPop: () async => false,
+      child: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            );
+          } else if (snapshot.hasData) {
+            print(snapshot.data?.email);
+            return HODRecords();
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text('Something went wrong.'),
+            );
+          } else {
+            return const MyLogin();
+          }
+        },
+      ),
+    ),
+  );
+}
