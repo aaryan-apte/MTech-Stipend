9435a059e0553c897f8bb8fa1326adaa7553d231
Akshay Potkhule <abpotkhule_b21@it.vjti.ac.in>
2023-05-01 15:49:35 +0530

    done

--- lib/pages/supervisor/attendance.dart
+++ lib/pages/supervisor/attendance.dart
@@ -20,8 +20,8 @@ class _StudentListState extends State<StudentList> {
   final studentPath = FirebaseFirestore.instance
       .collection("supervisor")
       .doc("muk") // TODO: Idhar EMAIL DAALNA HAI
-      .collection("students")
-      .doc("attendance")
+      .collection("attendance")
+      .doc("cs")
       .collection("${DateTime.now().month}-${DateTime.now().year}");
 
   String getStudentName(String email) {
@@ -33,220 +33,112 @@ class _StudentListState extends State<StudentList> {
     return name;
   }
 
-  List<Student> students = [
-    Student(name: "Alice", hours: 0),
-    Student(name: "Bob", hours: 0),
-    Student(name: "Charlie", hours: 0),
-  ];
-
-  final num _count = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Attendance'),
       ),
-
+      backgroundColor: Colors.white,
       body: StreamBuilder<QuerySnapshot>(
         stream: studentPath.snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        builder: (context, snapshot) {
           if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           }
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           }
-          if(snapshot.hasData){
-            // final docData = ;
-            return  const Text("");
+          if (snapshot.hasData && snapshot.data != null) {
+            print("Total Documents: ${snapshot.data!.docs.length}");
+            if (snapshot.data!.docs.isNotEmpty) {
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final Map<String, dynamic>? docData =
+                      snapshot.data?.docs[index].data()
+                          as Map<String, dynamic>?;
+                  if (docData!.isEmpty) {
+                    return const Text(
+                      "Document is Empty",
+                      textAlign: TextAlign.center,
+                    );
+                  }
 
+                  String name = docData["name"];
+                  num totalAttendance = docData["totalAttendance"];
+                  num week1 = docData["1"];
+                  num week2 = docData["2"];
+                  num week3 = docData["3"];
+                  num week4 = docData["4"];
+                  String email = docData["email"];
+                  print(email);
+
+                  return Container(
+                    margin: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.blue[100],
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: ListTile(
+                        title:Text(name),
+                        subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 5.0), // add some spacing between the widgets
+                          Text("Total Attendance: $totalAttendance"),
+                          Text("Week 1: $week1"),
+                          Text("Week 2: $week2"),
+                          Text("Week 3: $week3"),
+                          Text("Week 4: $week4"),
+                        ],
+                      ),
+                      trailing:Row(
+                        mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              color: Colors.green,
+                              focusColor: Colors.lightGreen,
+                              icon: const Icon(Icons.add),
+                              onPressed: () {
+                                print(getWeekNumber());
+                                studentPath
+                                    .doc(docData["email"])
+                                    .update({"${getWeekNumber()}": docData["${getWeekNumber()}"] + 1,"totalAttendance": totalAttendance+1});
+                              },
+                            ),
+                            IconButton(
+                              color: Colors.red,
+                              icon: const Icon(Icons.remove),
+                              onPressed: () {
+                                studentPath.doc(docData["email"]).update({
+                                  "${getWeekNumber()}":
+                                      docData["${getWeekNumber()}"] - 1,
+                                      "totalAttendance": totalAttendance-1
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                    ),
+                  );
+                },
+              );
+            }
           }
-          return const SizedBox();
-          // return ListView(
-          //   children: snapshot.data!.docs.map((DocumentSnapshot document) {
-          //     return StreamBuilder<DocumentSnapshot>(
-          //       stream: FirebaseFirestore.instance
-          //           .collection('students')
-          //           .doc(document.id)
-          //           .collection('attendance')
-          //           .doc('${DateTime.now().month}-${DateTime.now().year}')
-          //           .snapshots(),
-          //       builder: (BuildContext context,
-          //           AsyncSnapshot<DocumentSnapshot> attendanceSnapshot) {
-          //         if (attendanceSnapshot.hasError) {
-          //           return Text('Error: ${attendanceSnapshot.error}');
-          //         }
-          //         if (attendanceSnapshot.connectionState ==
-          //             ConnectionState.waiting) {
-          //           return const Center(child: Text('Loading...'));
-          //         }
-          //         var totalAttendance =
-          //             attendanceSnapshot.data?.get('totalAttendance') ?? 0;
-          //         return ListTile(
-          //           title: Text(document.id),
-          //           subtitle: Text('Total Attendance: $totalAttendance'),
-          //         );
-          //       },
-          //     );
-          //   }).toList(),
-          // );
+          return Container();
         },
       ),
-      // body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-      //   stream: studentPath.snapshots(),
-      //   builder: (context,
-      //       AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-      //     if (!snapshot.hasData) {
-      //       return const Center(
-      //         child: Text("No applications till date."),
-      //       );
-      //     }
-      //     if (snapshot.hasError) {
-      //       return Column(
-      //         children: [
-      //           const SizedBox(height: 10.0),
-      //           Container(
-      //             width: double.infinity,
-      //             height: 30.0,
-      //             color: Colors.white,
-      //             child: Center(
-      //               child: Text(snapshot.error.toString()),
-      //             ),
-      //           ),
-      //         ],
-      //       );
-      //     }
-      //     if (snapshot.connectionState == ConnectionState.waiting) {
-      //       return Center(
-      //         child: CircularProgressIndicator(
-      //           backgroundColor: Colors.white,
-      //           color: Colors.blue[900],
-      //         ),
-      //       );
-      //     }
-      //     if (snapshot.data!.docs.isNotEmpty) {
-      //       return ListView.builder(
-      //         itemBuilder: (context, int index) {
-      //           final documents = snapshot.data?.docs;
-      //           String emailStudent = documents![index].id.toString();
-      //           return StreamBuilder<DocumentSnapshot>(
-      //               stream: studentPath
-      //                   .doc(emailStudent)
-      //                   .collection("attendance")
-      //                   .doc("${DateTime.now().month}-${DateTime.now().year}")
-      //                   .snapshots(),
-      //               builder:
-      //                   (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-      //                 if (snapshot.data!.exists) {
-      //                   Map<String, dynamic> docData =
-      //                       snapshot.data!.data() as Map<String, int>;
-      //                   if (docData.isNotEmpty) {
-      //                     int totalAttendance = docData[0]["totalAttendance"];
-      //                     return Text(
-      //                         "Student name: ${getStudentName(emailStudent)}\n"
-      //                         "Attendance: $totalAttendance");
-      //                   }
-      //                 }
-      //                 // else{
-      //                 return SizedBox();
-      //                 // }
-      //               });
-      //         },
-      //         itemCount: snapshot.data?.docs.length,
-      //       );
-
-      // return ListView.builder(itemBuilder: (context, int index){
-
-      //   String studentName = getStudentName(emailStudent);
-      //
-      //   Map<String, dynamic> docData =
-      //   snapshot.data!.docs[index].data();
-      //   return Column(
-      //     children: [
-      //       Text(studentName),
-      //       Text(
-      //
-      //       )
-      //     ],
-      //   );
-      // },
-      // itemCount: snapshot.data!.docs.length,);
-      //     } else {
-      //       return SizedBox();
-      //     }
-      //   },
-      // ),
-      // body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-      //   stream: studentPath.snapshots(),
-      // builder: (context, QuerySnapshot<Map<String, dynamic>>){
-      //
-      //   },
-      // ),
-      // body: ListView.builder(
-      //   itemCount: students.length,
-      //   itemBuilder: (context, index) {
-      //     return Card(
-      //       shape:
-      //           RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-      //       elevation: 3,
-      //       color: Colors.indigo[700],
-      //       child: ListTile(
-      //         title: Text(students[index].name,
-      //             style: const TextStyle(color: Colors.white)),
-      //         subtitle: Text("Hours: ${students[index].hours}",
-      //             style: const TextStyle(color: Colors.white)),
-      //         trailing: Row(
-      //           mainAxisSize: MainAxisSize.min,
-      //           children: [
-      //             IconButton(
-      //               color: const Color.fromARGB(255, 67, 248, 73),
-      //               icon: const Icon(Icons.add),
-      //               onPressed: () {
-      //                 setState(() {
-      //                   students[index].hours += 0.5;
-      //                   _count += 0.5;
-      //                 });
-      //               },
-      //             ),
-      //             IconButton(
-      //               color: const Color.fromARGB(255, 248, 31, 16),
-      //               icon: const Icon(Icons.remove),
-      //               onPressed: () {
-      //                 setState(() {
-      //                   if (students[index].hours > 0) {
-      //                     students[index].hours -= 0.5;
-      //                     _count -= 0.5;
-      //                   }
-      //                 });
-      //               },
-      //             ),
-      //           ],
-      //         ),
-      //       ),
-      //     );
-      //   },
-      // ),
-      // BottomAppBar(
-      //   child: SizedBox(
-      //     height: 50,
-      //     child: Row(
-      //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //       children: [
-      //         Text('Total Count: $_count'),
-      //         Text('Week: ${getWeekNumber()}'),
-      //       ],
-      //     ),
-      //   ),
-      // ),
     );
   }
 
   int getWeekNumber() {
     var now = DateTime.now();
-    var startOfYear = DateTime(now.year, 1, 1);
-    var firstMonday =
-        startOfYear.subtract(Duration(days: startOfYear.weekday - 1));
+    var startOfMonth = DateTime(now.year, now.month, 1);
+    var currentWeekday = now.weekday;
+    var firstMonday = startOfMonth
+        .add(Duration(days: currentWeekday > 1 ? 8 - currentWeekday : 1));
     var currentMonday = now.subtract(Duration(days: now.weekday - 1));
     var weekNumber = currentMonday.difference(firstMonday).inDays ~/ 7 + 1;
     return weekNumber;
